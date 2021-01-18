d96fd1d33caaad13c51a05af363fe1a3f6fe2706
Sergej Sintschilin <seregpie@gmail.com>
2021-01-18 11:32:26 +0100

    restructure

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,5 @@
+import {babel} from '@rollup/plugin-babel';
 import {terser} from 'rollup-plugin-terser';
-import babel from '@rollup/plugin-babel';
-import nodeResolve from '@rollup/plugin-node-resolve';
-import replace from '@rollup/plugin-replace';
 
 import {main} from './package.json';
 
@@ -13,8 +11,6 @@ export default {
 	external: Object.keys(globals),
 	input: 'src/index.js',
 	plugins: [
-		nodeResolve(),
-		replace({'process.env.NODE_ENV': '"production"'}),
 		babel({
 			babelHelpers: 'bundled',
 			presets: ['@babel/preset-env'],
---
+++ src/components/VueResizeSensor.js
@@ -0,0 +1,88 @@
+import {
+	defineComponent,
+	h,
+	isVue2,
+	onActivated,
+	onMounted,
+	ref,
+	watch,
+} from 'vue-demi';
+
+export default defineComponent({
+	name: 'VueResizeSensor',
+	props: {
+		tag: {
+			type: String,
+			default: 'div',
+		},
+	},
+	setup(props, {
+		emit,
+		refs,
+		slots,
+	}) {
+		let widthRef = ref(0);
+		let heightRef = ref(0);
+		watch([widthRef, heightRef], ([width, height]) => {
+			emit('resize', {width, height});
+		});
+		let elRef = ref(null);
+		let updateSize = (() => {
+			let el = isVue2 ? refs.el : elRef.value;
+			if (el) {
+				widthRef.value = el.offsetWidth;
+				heightRef.value = el.offsetHeight;
+			}
+		});
+		onActivated(updateSize);
+		onMounted(updateSize);
+		let onLoad = (event => {
+			event.target.contentWindow.addEventListener('resize', updateSize);
+		});
+		return (() => {
+			let {tag} = props;
+			return h(
+				tag,
+				{
+					style: {
+						position: 'relative',
+					},
+					ref: isVue2 ? 'el' : elRef,
+				},
+				[
+					h(
+						'iframe',
+						{
+							style: {
+								border: 'none',
+								height: '100%',
+								opacity: 0,
+								position: 'absolute',
+								width: '100%',
+							},
+							...(isVue2
+								? {
+									on: {
+										load: onLoad,
+									},
+								}
+								: {
+									onLoad,
+								}
+							),
+						},
+					),
+					...(() => {
+						let slot = slots.default;
+						if (slot) {
+							let width = widthRef.value;
+							let height = heightRef.value;
+							return slot({width, height});
+						}
+						return [];
+					})(),
+				],
+			);
+		});
+	},
+});
